467d6577103d5b2c910b838b206427b23155ce2c
iszagb <iszael.gonzalez@alumnos.udg.mx>
2019-02-21 20:40:10 -0600

    Se cambio nombre en welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Punto de Venta
                 </div>
 
                 <div class="links">
