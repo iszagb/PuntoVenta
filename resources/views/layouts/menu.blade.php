e73a762b736d376c68ecf0eed937aced096bd17b
iszagb <iszael.gonzalez@alumnos.udg.mx>
2019-03-14 01:28:58 -0600

    Se implemento layout y se modifico al sistema en web

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,41 @@
+<div class="menu-trigger">
+  <span></span>
+  <span></span>
+  <span></span>
+</div>
+<header class="fixed-menu">
+  <span class="menu-close"><i class="fa fa-times"></i></span>
+  <div class="menu-header">
+    <div class="logo d-flex justify-content-center">
+      <img src="img/logo.png" alt="">
+    </div>
+  </div>
+  <div class="nav-wraper">
+    <div class="navbar">
+      <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link active" href="index.html"> home</a></li>
+        <li class="nav-item"><a class="nav-link" href="about-us.html">Desarrolladores</a></li>
+        <li class="nav-item"><a class="nav-link" href="menu.html">menu</a></li>
+        <li class="nav-item"><a class="nav-link" href="book-table.html">Book
+            Table</a></li>
+        <li class="nav-item"><a class="nav-link" href="Chefs.html">Chefs</a></li>
+        <li class="nav-item submenu dropdown">
+          <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true"
+           aria-expanded="false">Pages</a>
+          <ul class="dropdown-menu">
+            <li class="nav-item"><a class="nav-link" href="elements.html">element</a></li>
+          </ul>
+        </li>
+        <li class="nav-item submenu dropdown">
+          <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+           aria-expanded="false">Blog</a>
+          <ul class="dropdown-menu">
+            <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
+            <li class="nav-item"><a class="nav-link" href="single-blog.html">Blog Details</a></li>
+          </ul>
+        </li>
+        <li class="nav-item"><a class="nav-link" href="contact.html">contact</a></li>
+      </ul>
+    </div>
+  </div>
+</header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,115 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="img/favicon.png" type="image/png">
+	<title>Punto de venta</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+	<link rel="stylesheet" href="{{asset('vendors/linericon/style.css')}}">
+	<link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
+	<link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
+	<link rel="stylesheet" href="{{asset('vendors/owl-carousel/owl.carousel.min.css')}}">
+	<link rel="stylesheet" href="{{asset('vendors/lightbox/simpleLightbox.css')}}">
+	<link rel="stylesheet" href="{{asset('vendors/nice-select/css/nice-select.css')}}">
+	<link rel="stylesheet" href="{{asset('vendors/jquery-ui/jquery-ui.css')}}">
+	<link rel="stylesheet" href="{{asset('vendors/animate-css/animate.css')}}">
+	<!-- main css -->
+	<link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
 
-        <title>Laravel</title>
+<body>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+	<!--================ Start Header Menu Area =================-->
+	@include('layouts.menu')
+	<!--================ End Header Menu Area =================-->
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+	<div class="site-main">
+		<!--================ Start Home Banner Area =================-->
+		<section class="home_banner_area">
+			<div class="banner_inner">
+				<div class="container-fluid no-padding">
+					<div class="row fullscreen">
 
-            .full-height {
-                height: 100vh;
-            }
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Start banner bottom -->
+		<div class="row banner-bottom align-items-center justify-content-center">
+			<div class="col-lg-8">
+				<div class="banner_content">
+					<div class="row d-flex align-items-center">
+						<div class="col-lg-8 col-md-12">
+							<p class="top-text">Punto Venta en linea</p>
+							<h1>Punto Venta en linea</h1>
+							<p>Sistema enfocado a negocios nuevos que buscan un software de punto de venta enconomico, con label
+                  facilidad de modificarlo a su gusto con una interfaz amigable.</p>
+						</div>
+						<div class="col-lg-4 col-md-12">
+							<div class="banner-btn">
+								<a class="primary-btn text-uppercase" href="#">Desarrolladores</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End banner bottom -->
+		<!--================ End Home Banner Area =================-->
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+		<!--================ Start Breakfast Area =================-->
+		<div class="breakfast-area section_gap_top">
+			<div class="container">
+				<div class="row align-items-center justify-content-center">
+					<div class="col-lg-5">
+						<div class="left-content">
+							<h1>Un punto de venta configurable a tu gusto </h1>
+							<p>Mediante nuuestro sistema te permitimos modificar el menu para que agregues
+                  los productos que quieras, sin necesidad de preocuparte por llamar a un tecnico
+                    para que te ayude</p>
+							<p>Todo esto para que no tengas que gastar tanto y puedas emprender tu negocios
+                  propiamente!</p>
+							<a href="#" class="primary-btn">Ir a menu</a>
+						</div>
+					</div>
+					<div class="col-lg-6 offset-lg-1">
+						<div class="right-img">
+							<img class="img1 img-fluid" src="{{asset('img/food/homeguy.jpg')}}" alt="">
+							<img class="img2 img-fluid" src="{{asset('img/food/tech.png')}}" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--================ End Breakfast Area =================-->
 
-            .position-ref {
-                position: relative;
-            }
+		<!--================ Start Lunch Area =================-->
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+		<!--================ Start Footer Area =================-->
+	</div>
 
-            .content {
-                text-align: center;
-            }
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
+	<script src="{{asset('js/popper.js')}}"></script>
+	<script src="{{asset('js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('js/stellar.js')}}"></script>
+	<script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+	<script src="{{asset('vendors/lightbox/simpleLightbox.min.js')}}"></script>
+	<script src="{{asset('vendors/nice-select/js/jquery.nice-select.min.js')}}"></script>
+	<script src="{{asset('vendors/owl-carousel/owl.carousel.min.js')}}"></script>
+	<script src="{{asset('vendors/jquery-ui/jquery-ui.js')}}"></script>
+	<script src="{{asset('js/jquery.ajaxchimp.min.js')}}"></script>
+	<script src="{{asset('vendors/counter-up/jquery.waypoints.min.js')}}"></script>
+	<script src="{{asset('vendors/counter-up/jquery.counterup.js')}}"></script>
+	<script src="{{asset('js/mail-script.js')}}"></script>
+	<!--gmaps Js-->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+	<script src="{{asset('js/gmaps.min.js')}}"></script>
+	<script src="{{asset('js/theme.js')}}"></script>
+</body>
 
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Punto de Venta
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
 </html>
