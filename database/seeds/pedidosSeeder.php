2dbbcccf72073920c472f62554e2b03efff45341
iszagb <iszael.gonzalez@alumnos.udg.mx>
2019-04-02 15:48:47 -0600

    se agrego seeder de pedidos

---
+++ database/seeds/pedidosSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class pedidosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('pedidos')->insert([
+          'mesa' => 'mesa2',
+          'platillo' => 'hot dog',
+          'cantidad' => 1,
+        ]);
+    }
+}
